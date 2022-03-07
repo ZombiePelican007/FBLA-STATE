fca9482d06b81075995b15a778a363516ecc53e7
ZombiePelican007 <72618817+ZombiePelican007@users.noreply.github.com>
2022-03-06 18:43:53 -0700

    added new imgs for iphone

--- JavaScript/BGShuffle.js
+++ JavaScript/BGShuffle.js
@@ -1,5 +1,5 @@
 function bgshuffle() {
-    var randomnum = Math.floor(Math.random() * (4 - 1)) + 1;
+    var randomnum = Math.floor(Math.random() * (8)) + 1;
     if (window.innerWidth > "750") {
         document.getElementById("bgchange").style.background = "url(Imgs/landscape" + randomnum + ".jpg)";
 
