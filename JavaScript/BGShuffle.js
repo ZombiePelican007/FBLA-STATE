177f76a3a01e66e144bbde8604abc69c434d79fc
ZombiePelican007 <72618817+ZombiePelican007@users.noreply.github.com>
2022-03-06 18:05:40 -0700

    random img switch fix for js

--- JavaScript/BGShuffle.js
+++ JavaScript/BGShuffle.js
@@ -1,9 +1,9 @@
 function bgshuffle() {
     var randomnum = Math.floor(Math.random() * (4 - 1)) + 1;
     if (window.innerWidth > "750") {
-        document.getElementById("bgchange").style.background = "url(../Imgs/landscape" + randomnum + ".jpg)";
+        document.getElementById("bgchange").style.background = "url(Imgs/landscape" + randomnum + ".jpg)";
 
     } else {
-        document.getElementById("bgchange").style.background = "url(../Imgs/landscape" + randomnum + ".jpg)";
+        document.getElementById("bgchange").style.background = "url(Imgs/landscape" + randomnum + ".jpg)";
     }
 }
