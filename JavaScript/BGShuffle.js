b8106178b02c4527904ef3edeeb4af7b6b3bf4a7
ZombiePelican007 <72618817+ZombiePelican007@users.noreply.github.com>
2022-03-06 23:38:05 -0700

    fix

--- JavaScript/BGShuffle.js
+++ JavaScript/BGShuffle.js
@@ -1,3 +1,11 @@
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
 function bgshuffle() {
     var randomnum = Math.floor(Math.random() * (8)) + 1;
     if (window.innerWidth > "750") {
@@ -5,5 +13,14 @@ function bgshuffle() {
 
     } else {
         document.getElementById("bgchange").style.background = "url(Imgs/portrait" + randomnum + ".jpg)";
-    }
-}
+    };
+};
+
+async function testimonialswap() {
+    const testname = ['"WOW Just WOW"', "cool"];
+    const test = ["Not only did they ask me for creative input every step of the way but I also got to learn about Cinematoghraphy from their amazing crew!", "test"];
+    for (let i = 0; i < test.length; i++) {
+        document.getElementById("testimonialname").innerHTML = testname[i];
+        document.getElementById("testimonial").innerHTML = test[i];
+    };
+};
